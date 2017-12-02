eef1e28e06578cfe76d36416bef231ab2eb0c98e
Dan Hou <31412607+doradhou@users.noreply.github.com>
2017-12-01 23:43:30 -0500

    tooltip

--- src/bestat/test.py
+++ src/bestat/test.py
@@ -1,7 +1,17 @@
 from django.test import TestCase, Client
 from bestat.models import Neighbor
 
-class TodoListModelsTest(TestCase):
-    def test_simple_add(self):
-        self.assertTrue(Neighbor.objects.all().count() == 0)
 
+class TodoListTest(TestCase):
+    # Seeds the test database with data we obtained
+    # fixtures = ['sample-data']  # from python manage.py dumpdata
+
+    def test_home_page(self):  # Tests that a GET request to home page
+        client = Client()  # results in an HTTP 200 (OK) response.
+        response = client.get('/bestat/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_city_page(self):  # Tests that a GET request to map
+        client = Client()  # results in an HTTP 200 (OK) response.
+        response = client.get('/bestat/map?name=Pittsburgh')
+        self.assertEqual(response.status_code, 200)
